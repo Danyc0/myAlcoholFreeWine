a5b868bb027e032e5a3d9cd303e7da919e8a837e
Daniel <dac46@aber.ac.uk>
2015-12-05 22:30:09 +0000

    Added A and Z wines

--- ruby/db/seeds.rb
+++ ruby/db/seeds.rb
@@ -8,7 +8,35 @@
 
 5.times do |i|
     Wine.create(
+        name: "awine ##{i}",
+        image: "wine1.jpg",
+        short_description: "my wine one",
+        long_description: "my long wine one",
+        origin: "france",
+        grape: "sauvignon blanc",
+        vegitarian: true,
+        size: 500,
+        price: 6.99
+        )
+end
+
+30.times do |i|
+    Wine.create(
         name: "wine ##{i}",
+        image: "wine1.jpg",
+        short_description: "my wine one",
+        long_description: "my long wine one",
+        origin: "france",
+        grape: "sauvignon blanc",
+        vegitarian: true,
+        size: 500,
+        price: 6.99
+        )
+end
+
+5.times do |i|
+    Wine.create(
+        name: "zwine ##{i}",
         image: "wine1.jpg",
         short_description: "my wine one",
         long_description: "my long wine one",
