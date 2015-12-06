9356a99893271880a7dd6f49e60fdac40959956f
Daniel <dac46@aber.ac.uk>
2015-12-06 18:04:33 +0000

    Added supplier to the search

--- ruby/app/models/wine.rb
+++ ruby/app/models/wine.rb
@@ -1,5 +1,5 @@
 class Wine < ActiveRecord::Base
     def self.search(search)
-        Wine.where('name like :value OR short_description like :value OR long_description like :value OR origin like :value OR grape like :value OR size like :value OR price like :value', value: "%#{search}%")
+        Wine.where('name like :value OR short_description like :value OR long_description like :value OR origin like :value OR grape like :value OR size like :value OR price like :value OR supplier like :value', value: "%#{search}%")
     end
 end
