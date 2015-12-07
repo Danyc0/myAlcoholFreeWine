b8128990304a5ff02d0d28ed228eea4b1d7f99ae
Daniel <dac46@aber.ac.uk>
2015-12-07 07:37:00 +0000

    Added compare functionality for two suppliers

--- ruby/app/models/wine.rb
+++ ruby/app/models/wine.rb
@@ -8,13 +8,45 @@ class Wine < ActiveRecord::Base
     def self.refresh
         Wine.delete_all
 
-        response = RestClient.get 'http://localhost:9000/wines'
-        result = JSON.parse(response)
-        puts result.to_json
-        for bottle in result
-            bottle[:supplier] = :supplier1
+        response1 = RestClient.get 'http://localhost:9000/wines'
+        response2 = RestClient.get 'http://localhost:9001/wines'
+
+        result1 = JSON.parse(response1)
+        result2 = JSON.parse(response2)
+
+        duplicate_wines = []
+
+        result1.each_with_index {|bottle1, index1|
+            bottle1[:supplier] = :supplier1
+            result2.each_with_index{|bottle2, index2|
+                bottle2[:supplier] = :supplier2
+
+                if bottle1['id'] == bottle2['id']
+                    if bottle1['price'] < bottle2['price']
+                        duplicate_wines << bottle1
+                    else
+                        duplicate_wines << bottle2
+                    end
+                    
+                    result1.delete_at(index1)
+                    result2.delete_at(index2)
+                end
+            }
+        }
+
+        result1.each{|bottle1|
+            bottle1[:supplier] = :supplier1
+        }
+        result2.each{|bottle2|
+            bottle2[:supplier] = :supplier2
+        }
+
+        temp_wines = duplicate_wines.concat result1
+        final_wines = temp_wines.concat result2
+        final_wines.each {|bottle|
+            puts bottle
             @wine = Wine.create(bottle)
             @wine.save
-        end
+        }
     end
 end
