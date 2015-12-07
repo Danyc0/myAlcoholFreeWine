93f9c50f9f6a8760db2797124ed532a5b981bff9
Daniel <dac46@aber.ac.uk>
2015-12-07 13:55:42 +0000

    Added tests

--- ruby/test/controllers/shopping_carts_controller_test.rb
+++ ruby/test/controllers/shopping_carts_controller_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class ShoppingCartsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "add to basket" do
+    create id => 0, num_wine => 1
+    assert_not_null @shopping_cart
+  end
+
+  test "clear basket" do
+    create id => 0, num_wine => 1
+    clear
+    if @shopping_cart.total_unique_items == 0
+        assert_true
+    else
+        assert_false
+    end
+  end
+
+  test "check checkout clears basket" do
+    create id => 0, num_wine => 1
+    checkout
+    if @shopping_cart.total_unique_items == 0
+        assert_true
+    else
+        assert_false
+    end
+  end
 end
--- ruby/test/controllers/wines_controller_test.rb
+++ ruby/test/controllers/wines_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class WinesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "find wines" do
+     find :wine
+     assert_response :success
+     assert_not_nil assigns(:wines)
+  end
+
+  test "show all wines" do
+    index
+    assert_response :success
+    assert_not_nil assigns(:wines)
+  end
+
+  test "finds wine called wine" do
+     find :awine
+     assert_response :success
+     if @response.length > 0
+        assert_true
+     else
+        assert_false
+     end
+  end
 end
